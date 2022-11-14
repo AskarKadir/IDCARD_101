c54a4983c373ad5335b1b74bd9c590a14af1ff4c
AskarKadir <askarkadir07@gmail.com>
2022-11-14 22:11:33 +0700

    inisialisasi project

---
+++ src/test/java/com/example/PWSHome/PwsHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PWSHome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwsHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
