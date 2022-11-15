e7ac892c64211b2be24b26f7c27ce3b9f09c0206
AskarKadir <askarkadir07@gmail.com>
2022-11-15 17:26:26 +0700

    merubah format tanggal yang diterima dan mengolah gambar menjadi STRINGBASE64 serta mencoba mengiri ke halaman idcard

--- src/main/java/com/example/PWSHome/myController.java
+++ src/main/java/com/example/PWSHome/myController.java
@@ -4,10 +4,31 @@
  */
 package com.example.PWSHome;
 
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.Date;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
 /**
  *
  * @author Asus
  */
+@Controller
 public class myController {
     
+    @RequestMapping("/senddata")
+    public String olahData(
+            @RequestParam("nmlngkp") String namalengkap,
+            @RequestParam("nmpgln") String namapanggilan,
+            @RequestParam("tgl") @DateTimeFormat(pattern = "yyyy-MM-dd") Date tanggal,
+            @RequestParam("ft") MultipartFile file, Model model) throws 
+            IOException, ParseException
+    {
+        return "idcard";
+    }
 }
