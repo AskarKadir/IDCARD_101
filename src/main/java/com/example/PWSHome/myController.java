1345521f1636290920befafae7077dc9b5c77a80
AskarKadir <askarkadir07@gmail.com>
2022-11-15 18:37:00 +0700

    memperbaiki eror yang terjadi ketika menerima data

--- src/main/java/com/example/PWSHome/myController.java
+++ src/main/java/com/example/PWSHome/myController.java
@@ -6,7 +6,9 @@ package com.example.PWSHome;
 
 import java.io.IOException;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,14 +23,26 @@ import org.springframework.web.multipart.MultipartFile;
 @Controller
 public class myController {
     
-    @RequestMapping("/senddata")
+    @RequestMapping("/input")
     public String olahData(
             @RequestParam("nmlngkp") String namalengkap,
             @RequestParam("nmpgln") String namapanggilan,
             @RequestParam("tgl") @DateTimeFormat(pattern = "yyyy-MM-dd") Date tanggal,
-            @RequestParam("ft") MultipartFile file, Model model) throws 
+            @RequestParam("image") MultipartFile file, 
+            Model model) throws 
             IOException, ParseException
     {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/M/yyyy");  
+        String konvtgl = formatter.format(tanggal);
+        
+        String blob = Base64.encodeBase64String(file.getBytes());
+        String gambar = "data:image/jpeg;base64,".concat(blob);
+        
+        
+        model.addAttribute("nama", namalengkap);
+        model.addAttribute("panggilan", namapanggilan);
+        model.addAttribute("tanggal", konvtgl);
+        model.addAttribute("fotoorang", gambar);
         return "idcard";
     }
 }
